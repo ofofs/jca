084959ecd77de87d08cac0fdf5c02eb508ace3f8
康永敢 <kangyonggan@gmail.com>
2018-06-26 22:17:49 +0800

    @Cache createBefore

--- jca-core/src/main/java/com/github/ofofs/jca/processor/CacheProcessor.java
+++ jca-core/src/main/java/com/github/ofofs/jca/processor/CacheProcessor.java
@@ -1,5 +1,6 @@
 package com.github.ofofs.jca.processor;
 
+import com.github.ofofs.jca.annotation.Cache;
 import com.github.ofofs.jca.annotation.Handler;
 import com.github.ofofs.jca.annotation.Log;
 import com.github.ofofs.jca.handler.impl.MemoryCacheHandler;
@@ -8,9 +9,11 @@ import com.github.ofofs.jca.util.Sequence;
 import com.sun.tools.javac.code.Flags;
 
 import javax.annotation.processing.RoundEnvironment;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 日志注解处理器
+ * 缓存注解处理器
  *
  * @author kangyonggan
  * @since 6/22/18
@@ -22,13 +25,13 @@ public class CacheProcessor extends AbstractJcaProcessor {
     }
 
     /**
-     * 处理日志注解
+     * 处理缓存注解
      */
     @Override
     protected void process() {
-        if (isEnable(Handler.Type.LOG)) {
+        if (isEnable(Handler.Type.CACHE)) {
             String fieldName = Sequence.nextString("field");
-            for (JcaMethod jcaMethod : getJcaMethods(Log.class)) {
+            for (JcaMethod jcaMethod : getJcaMethods(Cache.class)) {
                 process(jcaMethod, fieldName);
             }
         }
@@ -44,6 +47,28 @@ public class CacheProcessor extends AbstractJcaProcessor {
         // 给方法所在的类创建一个字段
         createField(jcaMethod.getJcaClass(), fieldName);
 
+        // 在方法第一行创建代码块
+        createBefore(jcaMethod, fieldName);
+    }
+
+    /**
+     * Object cacheValue = memoryCacheHandler.get(key);
+     * if (cacheValue != null) {return (returnType) cacheValue;}
+     *
+     * @param jcaMethod 方法
+     * @param fieldName 字段名
+     */
+    private void createBefore(JcaMethod jcaMethod, String fieldName) {
+        List<JcaObject> args = new ArrayList<>();
+        // TODO key
+        args.add(JcaCommon.getNull());
+
+        // Object cacheValue = memoryCacheHandler.get(key)
+        String varName = Sequence.nextString("var");
+        JcaVariable jcaVariable = new JcaVariable(Object.class, varName, JcaCommon.method(fieldName, "get", args));
+        jcaMethod.insert(jcaVariable);
+
+        // TODO
     }
 
     /**
